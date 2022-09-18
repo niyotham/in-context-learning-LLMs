c7c7183c02aa700440d79dfb64c7e877e9980f74
niyotham <66441805+niyotham@users.noreply.github.com>
2022-09-18 13:01:22 +0200

    entity extraction notebook added

--- scripts/processing.py
+++ scripts/processing.py
@@ -1,5 +1,5 @@
 from cProfile import label
-
+import pandas as pd
 
 def process_tokens(tokens):
     enty={}
@@ -13,3 +13,8 @@ def process_tokens(tokens):
     for k, v in enty.items():
         label += k+':'+v+"\n"
     return label
+
+def entity_extraction(self,test_df:pd.DataFrame):
+    test_doc = test_df.document.apply(
+        lambda x: x.replace("\n", " ")+'\n\nExtracted Text:').to_list()
+    return test_doc
